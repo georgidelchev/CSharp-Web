ce6c75c7189d8e3794333d6933aa2aaa8ca19ddf
george <alienguymc@gmail.com>
2021-03-01 13:21:02 +0200

    add

---
+++ 01 - [CSharp Web Basics]/02 - [Web Server - Asynchronous Processing]/HTTPProtocolDemo/StartUp.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HTTPProtocolDemo
+{
+    public class StartUp
+    {
+        const string NEW_LINE = "\r\n";
+
+        public static async Task Main(string[] args)
+        {
+
+            //await HttpRequest();
+
+            var tcpListener = new TcpListener(IPAddress.Loopback, 80);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                var tcpClient = tcpListener.AcceptTcpClient();
+
+                ProcessClientAsync(tcpClient);
+            }
+        }
+
+        private static async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            var networkStream = tcpClient.GetStream();
+
+            using (networkStream)
+            {
+
+                // TODO: Use buffer
+                var requestBytes = new byte[1000000];
+
+                var bytesRead = await networkStream.ReadAsync(requestBytes, 0, requestBytes.Length);
+
+                var request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
+
+                Thread.Sleep(10000);
+
+                //var responseText = "<h1>Hello Header</h1>";
+
+                string responseText = @"<form action='/Account/Login' method='post'> 
+                                        <input type=text name='username' /> 
+                                        <input type=password name='password' /> 
+                                        <input type=date name='date' /> 
+                                        <input type=submit value='login' /> 
+                                        </form>" + DateTime.Now;
+
+                var response = "HTTP/1.0 200 OK" +
+                               NEW_LINE +
+                               "Server: SoftUniServer/1.0" +
+                               NEW_LINE +
+                               "Content-Type: text/html" +
+                               NEW_LINE +
+                               $"Content-Length: {responseText.Length}" +
+                               NEW_LINE +
+                               NEW_LINE +
+                               responseText;
+
+                var responseBytes = Encoding.UTF8.GetBytes(response);
+
+                await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+
+                Console.WriteLine(request);
+                Console.WriteLine(new string('=', 60));
+            }
+        }
+
+        private static async Task HttpRequest()
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("https://softuni.bg/");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 01 - [CSharp Web Basics]/02 - [Web Server - Asynchronous Processing]/PrimeNumbersCounter/StartUp.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersCounter
+{
+    public class StartUp
+    {
+        static int Count = 0;
+
+        private static object lockObj = new object();
+
+        public static void Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+
+            var tasks = new List<Task>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                var task = Task.Run(async () =>
+                {
+                    var client = new HttpClient();
+
+                    var url = $"https://vicove.com/vic-{i}";
+
+                    var response = await client.GetAsync(url);
+
+                    var vic = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(vic.Length);
+                });
+
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine(sw.Elapsed);
+
+            Console.ReadLine();
+
+            // TaskTest();
+
+            // ThreadsWorkTest();
+
+            // PrintPrimeCount(1,10_000_000);
+
+            // ThreadException();
+
+            // FourThreadsWork();
+        }
+
+        private static void TaskTest()
+        {
+            var sw = Stopwatch.StartNew();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                    }
+                });
+            }
+
+            Console.WriteLine(sw.Elapsed);
+            Console.ReadLine();
+        }
+
+        private static void ThreadsWorkTest()
+        {
+            var sw = Stopwatch.StartNew();
+
+            var threads = new List<Thread>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var thread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                    }
+                });
+
+                thread.Start();
+
+                threads.Add(thread);
+            }
+
+            Console.WriteLine(sw.Elapsed);
+        }
+
+        private static void ThreadException()
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    throw new Exception();
+                }
+                catch (Exception)
+                {
+                }
+            }).Start();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                Console.WriteLine(input.ToUpper());
+            }
+        }
+
+        private static void FourThreadsWork()
+        {
+            var sw = Stopwatch.StartNew();
+
+            var thread = new Thread(() => PrintPrimeCount(1, 2_500_000));
+            thread.Start();
+
+            var thread2 = new Thread(() => PrintPrimeCount(2_500_001, 5_000_000));
+            thread2.Start();
+
+            var thread3 = new Thread(() => PrintPrimeCount(5_000_001, 7_500_000));
+            thread3.Start();
+
+            var thread4 = new Thread(() => PrintPrimeCount(7_500_001, 10_000_000));
+            thread4.Start();
+
+            thread.Join();
+            thread2.Join();
+            thread3.Join();
+            thread4.Join();
+
+            Console.WriteLine(Count);
+
+            Console.WriteLine(sw.Elapsed);
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+
+                Console.WriteLine(input.ToUpper());
+            }
+        }
+
+        private static void PrintPrimeCount(int min, int max)
+        {
+            for (int i = min; i <= max; i++)
+            {
+                var isPrime = true;
+
+                for (int j = 2; j < Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrime = false;
+
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    lock (lockObj)
+                    {
+                        Count++;
+                    }
+                }
+            }
+        }
+    }
+}

b969dc1e5e7f46a063ffbf160cdaabdf649cde1e
george <alienguymc@gmail.com>
2021-06-25 15:33:48 +0300

    add ctors to the skeleton of battlecards exam

--- SKELETONS CSHARP WEB BASICS/3 - [BattleCards Skeleton]/BattleCards/Data/ApplicationDbContext.cs
+++ SKELETONS CSHARP WEB BASICS/3 - [BattleCards Skeleton]/BattleCards/Data/ApplicationDbContext.cs
@@ -4,6 +4,15 @@ namespace BattleCards.Data
 {
     public class ApplicationDbContext : DbContext
     {
+		public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+		
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
